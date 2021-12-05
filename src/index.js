20ff1085c9d66010924d24e56cba332708341642
draganet <3417237276@qq.com>
2021-12-05 16:18:28 +0800

    代替Redux-init

--- src/index.js
+++ src/index.js
@@ -1,83 +1,127 @@
-import React, { useReducer } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
 import ReactDOM from "react-dom";
 
-const initFormData = {
-  name: "",
-  age: 18,
-  nationality: "汉族",
+const store = {
+  user: null,
+  books: null,
+  movies: null,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "patch":
-      return { ...state, ...action.formData };
-    case "reset":
-      return initFormData;
+    case "setUser":
+      return { ...state, user: action.user };
+    case "setBooks":
+      return { ...state, books: action.books };
+    case "setMovies":
+      return { ...state, movies: action.movies };
     default:
       throw new Error();
   }
 };
 
+const Context = createContext(null);
 
 const App = () => {
-  console.log("aaa");
-  const [formData, dispatch] = useReducer(reducer, initFormData);
-  const onSubmit = () => {};
-  const onReset = () => {
-    dispatch({ type: "reset" });
-  };
+  const [state, dispatch] = useReducer(reducer, store);
+
   return (
-    <form onSubmit={onSubmit} onReset={onReset}>
-      <div>
-        <label>
-          姓名
-          <input
-            value={formData.name}
-            onChange={(e) =>
-              dispatch({
-                type: "patch",
-                formData: { name: e.target.value },
-              })
-            }
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          年龄
-          <input
-            value={formData.age}
-            onChange={(e) =>
-              dispatch({
-                type: "reset",
-                formData: { age: e.target.name },
-              })
-            }
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          民族
-          <input
-            value={formData.nationality}
-            onChange={(e) =>
-              dispatch({
-                type: "patch",
-                formData: { nationality: e.target.value },
-              })
-            }
-          />
-        </label>
-      </div>
-      <div>
-        <button type="submit">提交</button>
-        <button type="reset">重置</button>
-      </div>
+    <Context.Provider value={{ state, dispatch }}>
+      <User />
       <hr />
-      {JSON.stringify(formData)}
-    </form>
+      <Books />
+      <hr />
+      <Movies />
+    </Context.Provider>
   );
 };
+const User = () => {
+  const { state, dispatch } = useContext(Context);
+  useEffect(() => {
+    ajax("/user").then((user) => {
+      dispatch({ type: "setUser", user });
+    });
+  }, []);
+  return (
+    <div>
+      <h1>个人信息</h1>
+      <div>name: {state.user ? state.user.name : ""}</div>
+    </div>
+  );
+};
+
+const Books = () => {
+  const { state, dispatch } = useContext(Context);
+  useEffect(() => {
+    ajax("/books").then((books) => {
+      dispatch({ type: "setBooks", books });
+    });
+  }, []);
+  return (
+    <div>
+      <h1>我的书籍</h1>
+      <ol>
+        {state.books
+          ? state.books.map((book) => <li key={book.id}>{book.name}</li>)
+          : "加载中"}
+      </ol>
+    </div>
+  );
+};
+
+const Movies = () => {
+  const { state, dispatch } = useContext(Context);
+  ajax("/movies").then((movies) => {
+    dispatch({ type: "setMovies", movies });
+  });
+  return (
+    <div>
+      <h1>我的电影</h1>
+      <ol>
+        {state.movies
+          ? state.movies.map((movies) => <li key={movies.id}>{movies.name}</li>)
+          : "加载中"}
+      </ol>
+    </div>
+  );
+};
+
+// 帮助函数
+// 假 ajax
+// 两秒钟后，根据 path 返回一个对象，必定成功不会失败
+function ajax(path) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (path === "/user") {
+        resolve({
+          id: 1,
+          name: "frank",
+        });
+      } else if (path === "/books") {
+        resolve([
+          {
+            id: 1,
+            name: "JavaScript 高级程序设计",
+          },
+          {
+            id: 2,
+            name: "你不知道的 JavaScript",
+          },
+        ]);
+      } else if (path === "/movies") {
+        resolve([
+          {
+            id: 1,
+            name: "卧虎藏龙",
+          },
+          {
+            id: 2,
+            name: "绿里奇迹",
+          },
+        ]);
+      }
+    }, 2000);
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
