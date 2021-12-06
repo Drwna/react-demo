f157f611ba7f4b05e80f8cacfbb18df2e7b4f177
draganet <3417237276@qq.com>
2021-12-06 23:43:48 +0800

    React.useRef--重新渲染

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ const App = () => {
   console.log("App执行了");
   const count = useRef(0);
   const [n, setN] = useState(0);
+  const [_, set_] = useState(null);
   const onClick = () => {
     setN((i) => i + 1);
+  };
+  const onClick2 = () => {
+    count.current += 1;
+    // 为了更新 ui
+    set_(Math.random());
+    console.log(count.current);
   };
   useEffect(() => {
     count.current += 1;
@@ -20,6 +27,7 @@ const App = () => {
       <hr />
       <div>
         <button onClick={onClick}>update n :{n}</button>
+        <button onClick={onClick2}>update count:{count.current}</button>
       </div>
     </div>
   );
