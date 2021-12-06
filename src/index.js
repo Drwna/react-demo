f254a704441469d57458d56a3367264d0165e96e
draganet <3417237276@qq.com>
2021-12-06 21:43:46 +0800

    React.useRef

--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
-import React, { memo, useMemo, useState } from "react";
+import React, { useRef, useState } from "react";
 import ReactDOM from "react-dom";
+import { useEffect } from "react/cjs/react.development";
 
+// 需要知道 App 执行了几次
 const App = () => {
   console.log("App执行了");
+  const count = useRef(0);
   const [n, setN] = useState(0);
-  const [m, setM] = useState(0);
   const onClick = () => {
     setN((i) => i + 1);
   };
-  // 这句重新执行了
-  const onClickChild = () => {};
-  // 改为 2
-  const onClickChild2 = useMemo(() => {
-    return () => {};
-  }, [m]);
+  useEffect(() => {
+    count.current += 1;
+    console.log("App执行了", count.current, "次");
+  });
   return (
     <div>
-      <h1>React.memo</h1>
+      <h1>React.useRef</h1>
       <hr />
       <div>
         <button onClick={onClick}>update n :{n}</button>
-        <Child2 data={m} onClick={onClickChild2} />
       </div>
     </div>
   );
 };
-
-function Child(prop) {
-  console.log("child 执行了");
-  console.log("假设这里还有大量代码");
-  return <div>child m: {prop.data} </div>;
-}
-const Child2 = memo(Child);
 
 ReactDOM.render(<App />, document.getElementById("root"));
