40da04d4de310280a3e8a4b59f28f5211e21b58d
draganet <3417237276@qq.com>
2021-12-06 20:28:51 +0800

    useLayoutEffect

--- src/index.js
+++ src/index.js
@@ -1,35 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const [n, setN] = useState(0);
-  const onClick = () => {
-    setN((i) => i + 1);
-  };
-  // useEffect(() => {
-  //   const id = setInterval(() => {
-  //     console.log("hi");
-  //   }, 2000);
-  //   return () => {
-  //     window.clearInterval(id);
-  //   };
-  // });
-  useEffect(() => {
-    console.log("第一次渲染后执行这句话");
-  }, []); // [] 里面的变量变化时执行 --> 不会执行
-  useEffect(() => {
-    console.log("n变化了");
-  }, [n]); // n 变化时执行
-  useEffect(() => {
-    console.log("任何一个 state 变化时都执行");
-  });
+  const [value, setValue] = useState(0);
+  // const onClick = () => {
+  //   setN((i) => i + 1);
+  // };
+  useLayoutEffect(() => {
+    document.querySelector("#x").innerText = "value: 999";
+  }, [value]);
   return (
-    <div>
-      <h1>useEffect</h1>
-      <hr />
-      n:{n}
-      <button onClick={onClick}>点击+1</button>
+    <div
+      id="x"
+      onClick={() => {
+        setValue(0);
+      }}
+    >
+      value:{value}
     </div>
+    // <div>
+    //   <h1>useEffect</h1>
+    //   <hr />
+    //   n:{n}
+    //   <button onClick={onClick}>点击+1</button>
+    // </div>
   );
 };
 
