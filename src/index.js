d960b5d69ecc4f322b549150d2444e86b4185358
draganet <3417237276@qq.com>
2021-12-06 21:36:41 +0800

    react.useMemo

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
-import React, { memo, useState } from "react";
+import React, { memo, useMemo, useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
+  console.log("App执行了");
   const [n, setN] = useState(0);
   const [m, setM] = useState(0);
   const onClick = () => {
     setN((i) => i + 1);
   };
-  const onClick2 = () => {
-    setM((i) => i + 1);
-  };
+  // 这句重新执行了
+  const onClickChild = () => {};
+  // 改为 2
+  const onClickChild2 = useMemo(() => {
+    return () => {};
+  }, [m]);
   return (
     <div>
       <h1>React.memo</h1>
       <hr />
       <div>
         <button onClick={onClick}>update n :{n}</button>
-        <Child data={m} />
-        {/* <Child2 data={m} /> */}
-        <button onClick={onClick2}>update m :{m}</button>
+        <Child2 data={m} onClick={onClickChild2} />
       </div>
     </div>
   );
 };
 
-// function Child(prop) {
-//   console.log("child 执行了");
-//   console.log("假设这里还有大量代码");
-//   return <div>child m: {prop.data} </div>;
-// }
-// const Child2 = memo(Child);
-
-const Child = memo((props) => {
+function Child(prop) {
   console.log("child 执行了");
   console.log("假设这里还有大量代码");
-  return <div>child m: {props.data} </div>;
-});
+  return <div>child m: {prop.data} </div>;
+}
+const Child2 = memo(Child);
 
 ReactDOM.render(<App />, document.getElementById("root"));
