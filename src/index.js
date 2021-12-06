7da153a3a97b4f8ff5d552fd0159fff8606cda1d
draganet <3417237276@qq.com>
2021-12-06 20:49:32 +0800

    react.memo()

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { memo, useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const [value, setValue] = useState(0);
-  // const onClick = () => {
-  //   setN((i) => i + 1);
-  // };
-  useLayoutEffect(() => {
-    document.querySelector("#x").innerText = "value: 999";
-  }, [value]);
+  const [n, setN] = useState(0);
+  const [m, setM] = useState(0);
+  const onClick = () => {
+    setN((i) => i + 1);
+  };
+  const onClick2 = () => {
+    setM((i) => i + 1);
+  };
   return (
-    <div
-      id="x"
-      onClick={() => {
-        setValue(0);
-      }}
-    >
-      value:{value}
+    <div>
+      <h1>React.memo</h1>
+      <hr />
+      <div>
+        <button onClick={onClick}>update n :{n}</button>
+        <Child data={m} />
+        {/* <Child2 data={m} /> */}
+        <button onClick={onClick2}>update m :{m}</button>
+      </div>
     </div>
-    // <div>
-    //   <h1>useEffect</h1>
-    //   <hr />
-    //   n:{n}
-    //   <button onClick={onClick}>点击+1</button>
-    // </div>
   );
 };
+
+// function Child(prop) {
+//   console.log("child 执行了");
+//   console.log("假设这里还有大量代码");
+//   return <div>child m: {prop.data} </div>;
+// }
+// const Child2 = memo(Child);
+
+const Child = memo((props) => {
+  console.log("child 执行了");
+  console.log("假设这里还有大量代码");
+  return <div>child m: {props.data} </div>;
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
