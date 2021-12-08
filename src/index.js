d393f577c08cd2c8ecf29d379d4d838b4722f369
draganet <3417237276@qq.com>
2021-12-08 18:03:46 +0800

    React.useImpreativeHandle-demo1

--- src/index.js
+++ src/index.js
@@ -1,48 +1,39 @@
-import React, {
-  useState,
-  useImperativeHandle,
-  useRef,
-  forwardRef,
-} from "react";
+import React, { useImperativeHandle, useRef, forwardRef } from "react";
 import ReactDOM from "react-dom";
 
-// 子组件
-const ChildComponent = forwardRef((props, ref) => {
-  const [count, setCount] = useState(0); //子组件定义内部变量count
-  //子组件定义内部函数 addCount
-  const addCount = () => {
-    setCount(count + 1);
-  };
-  //子组件通过useImperativeHandle函数，将addCount函数添加到父组件中的ref.current中
-  useImperativeHandle(ref, () => ({ addCount }));
+const Children = forwardRef((props, ref) => {
+  const inputRef = useRef(null);
+  useImperativeHandle(ref, () => ({
+    name: "子组件暴露给父组件的 name 属性",
+    focus: () => {
+      inputRef.current && inputRef.current.focus();
+    },
+  }));
   return (
-    <div>
-      {count}
-      <button onClick={addCount}>child</button>
-    </div>
+    <>
+      <input type="text" ref={inputRef} />
+    </>
   );
 });
 
-// 父组件
-function Imperative() {
-  const childRef = useRef(null); //父组件定义一个对子组件的引用
-
-  const clickHandle = () => {
-    childRef.current.addCount(); //父组件调用子组件内部 addCount函数
-  };
-
+const Parent = () => {
+  const el = useRef(null);
   return (
-    <div>
-      {/* 父组件通过给子组件添加 ref 属性，将childRef传递给子组件，
-            子组件获得该引用即可将内部函数添加到childRef中 */}
-      <ChildComponent ref={childRef} />
-      <button onClick={clickHandle}>child component do something</button>
-    </div>
+    <>
+      <Children ref={el} />
+      <button
+        onClick={() => {
+          console.log(el.current);
+        }}
+      >
+        获取子组件暴露给父组件的东西
+      </button>
+    </>
   );
-}
+};
 
 function App() {
-  return <Imperative />;
+  return <Parent />;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
