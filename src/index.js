852349ccc554a10822807e48d9d1774a7be2a69b
draganet <3417237276@qq.com>
2021-12-08 18:35:47 +0800

    React.useImpreativeHandle-demo1.1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React, { useImperativeHandle, useRef, forwardRef } from "react";
+import React, {
+  useImperativeHandle,
+  useRef,
+  forwardRef,
+  useState,
+} from "react";
 import ReactDOM from "react-dom";
 
-const Children = forwardRef((props, ref) => {
+const Children = forwardRef((props, cref) => {
+  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(0);
   const inputRef = useRef(null);
-  useImperativeHandle(ref, () => ({
-    name: "子组件暴露给父组件的 name 属性",
-    focus: () => {
-      inputRef.current && inputRef.current.focus();
-    },
-  }));
+
+  useImperativeHandle(
+    cref,
+    () => ({
+      name: "子组件暴露给父组件的 name 属性",
+      focus: () => {
+        inputRef.current && inputRef.current.focus();
+      },
+      count,
+    }),
+    [num]
+  );
+  /*
+先点击setCount按钮加到7，然后点击“获取子组件暴露给父组件的东西”按钮，
+此时打印出的count还是初使状态的0，只有点击了setNum按钮，（也就是说当num发生改变当时候），
+再次点击“获取子组件暴露给父组件的东西”按钮，此时打印出的count为改变后的7
+*/
   return (
     <>
+      <h3>count: {count}</h3>
+      <h3>num: {num}</h3>
       <input type="text" ref={inputRef} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        setCount
+      </button>
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        setCount
+      </button>
     </>
   );
 });
